9951e6104f10d8242e9a66021423505e3898e2d6
zhuyunlong <569433050@qq.com>
2019-08-08 09:17:14 +0800

    提交代码

---
+++ src/main/java/com/example/community/bean/GithubUser.java
@@ -0,0 +1,32 @@
+package com.example.community.bean;
+
+public class GithubUser {
+
+    public String name;
+    public Long id;
+    public String bio;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+}
--- src/main/java/com/example/community/controller/MyController.java
+++ src/main/java/com/example/community/controller/MyController.java
@@ -1,8 +1,15 @@
 package com.example.community.controller;
 
 
+import com.example.community.Provider.GithubProvider;
+import com.example.community.bean.GithubUser;
+import com.example.community.dto.AccessTokenDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.security.Provider;
 
 @Controller
 public class MyController {
@@ -11,4 +18,27 @@ public class MyController {
     public String index(){
         return "index";
     }
+
+
+    @Autowired
+    GithubProvider githubProvider;
+
+    @RequestMapping("/callback")
+    public String callback(@RequestParam(name="code") String code,
+                           @RequestParam(name="state") String state){
+
+        AccessTokenDTO accessTokenDTO=new AccessTokenDTO();
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri("http://localhost:8080/callback");
+        accessTokenDTO.setClient_id("edcf01497196ffdf1467");
+        accessTokenDTO.setState(state);
+        accessTokenDTO.setClient_secret("924cc4d6232a2eb9888e9d33f31100c059f52b27");
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getBio());
+
+        return "index";
+    }
 }
+
+
